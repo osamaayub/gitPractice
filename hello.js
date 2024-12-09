95c8a030a4d5cbe82114d7be5b3594c04cd6a92a
osamaayub <ayubosama416@gmail.com>
2024-12-09 14:10:11 +0500

    Revert "added console .log in hello.js file"
    This reverts commit 85e906f5fc86ced39c363f3ed0ebc6c7214c6f79.

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ console.log("Hello,Github")
 
 console.log("Hello to ,Github CrashCourse from Osama");
 
-console.log("This is bad coding practice");
+
 
 
 console.log("this is git & github tutorial")
